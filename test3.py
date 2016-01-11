05aff9447063e3b1145f3950888fb5c6417dccd5
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-10 19:03:15 -0500

    Improvements on the article parsing

--- test3.py
+++ test3.py
@@ -1,6 +1,7 @@
 import urllib.request
 from datetime import datetime
 import dateutil.parser
+import re
 from bs4 import BeautifulSoup
 
 # url = "http://www.reuters.com/article/us-eurozone-economy-dawn-analysis-idUSKCN0UO0DQ20160110?feedType=RSS&feedName=businessNews"
@@ -16,5 +17,18 @@ article_title = soup.find("h1", {"class": "article-headline"}).text
 article_date = dateutil.parser.parse(
     soup.find("span", {"class": "timestamp"}).text)
 article_location = soup.find("span", {"class": "location"}).text
-article_text = soup.find(id="articleText").text
+# article_text = soup.find(id="articleText").text
 
+article_paragraphs = [p.text.replace('\n',' ') for p in
+                      soup.find(id="articleText").find_all("p")]
+
+if article_paragraphs[0].startswith(article_location + ' '):
+    article_paragraphs[0] = article_paragraphs[0].replace(article_location + ' ','', 1) # at most once
+
+article_paragraphs[0] = re.sub(r'\w{3} \d{2} ','',article_paragraphs[0])
+
+last_paragraph = article_paragraphs[-1]
+if last_paragraph[0] == '(' and last_paragraph[-1] == ')':
+    article_paragraphs = article_paragraphs[:-1]
+
+article_text = "\n\n".join(article_paragraphs)
