c099c21b51d6e130769b0f505a0ff7212b8a2c21
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-10 20:52:25 -0500

    Corrections (regexes for edge cases)

--- test3.py
+++ test3.py
@@ -4,23 +4,28 @@ import dateutil.parser
 import re
 from bs4 import BeautifulSoup
 
-# url = "http://www.reuters.com/article/us-eurozone-economy-dawn-analysis-idUSKCN0UO0DQ20160110?feedType=RSS&feedName=businessNews"
-url = "http://www.reuters.com/article/china-orient-ipo-idUSL3N14C04620151223?feedType=RSS&feedName=marketsNews"
+url = "http://www.reuters.com/article/us-usa-stocks-idUSKBN0UM1C020160109"
 
 with urllib.request.urlopen(url) as response:
     html = response.read()
 
 soup = BeautifulSoup(html, "lxml")
 
-#Raw data
+# Raw data
 article_title = soup.find("h1", {"class": "article-headline"}).text
 article_date = dateutil.parser.parse(
     soup.find("span", {"class": "timestamp"}).text)
-article_location = soup.find("span", {"class": "location"}).text
-# article_text = soup.find(id="articleText").text
+article_location = soup.find("span", {"class": "location"})
+
+
+# Manual cleanup
+article_location = article_location.text if article_location else ''
 
 article_paragraphs = [p.text.replace('\n',' ') for p in
                       soup.find(id="articleText").find_all("p")]
+
+# Remove early and trailing whitespace
+article_paragraphs = [re.sub(r'^(\s*)(.+?)(\s*)$',r'\2',p) for p in article_paragraphs if p]
 
 if article_paragraphs[0].startswith(article_location + ' '):
     article_paragraphs[0] = article_paragraphs[0].replace(article_location + ' ','', 1) # at most once
