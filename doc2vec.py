06539eaa3bb48cd3bd06da83822907db39bfdac2
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-12 09:24:55 -0500

    Sotchastic batch method created, not tested.

--- doc2vec.py
+++ doc2vec.py
@@ -2,26 +2,55 @@ import collections
 import nltk
 import article_db
 import tensorflow as tf
+import random
 
 '''Returns a matrix of the encoding for each word in our vocabulary.'''
 stops = { '.',';',',' }        # Improve with NLTK
 pars = [nltk.word_tokenize(a.content) for a in article_db.fetch(10)]
-corpus = [[w.lower() for w in p if w not in stops] for p in pars]
+pars = [[w.lower() for w in p if w not in stops] for p in pars]
 words = [w for p in corpus for w in p]
-counter = collections.Counter(words) # Other data structure.
+counter = collections.Counter(words) # Other data structure?
 
-p_word = 50
-p_par = 150
-T_word = len(counter)
-T_par = len(corpus)
+# Assign an id to each word
+word_dict = {w: i for i,w in enumerate(counter)}
+
+T_word = len(word_dict)
+T_par = len(pars)
+
+def stochastic_batch(k_around, batch_size=1):
+    # Paragraph 1-hot vector
+    p = random.randrange(T_par)
+    t_par = [0] * T_par
+    t_par[p] = 1
+
+    par = pars[p]
+    l = len(par)
+
+    # Center 1-hot vector
+    c_pos = random.randrange(k_around, l-k_around)
+    c = par[c_pos]
+    t_c = [0] * T_word
+    t_c[word_dict[c]] = 1
+
+    # Surrounding words 1-hot matrix
+    ws = [par[i] for i in par[c_pos-k_around:c_pos+k_around] if i is not c_pos]
+    t_ws = [[0] * (2*k_around) for _ in range(T_word)]
+    for j in range(T_word):
+        t_ws[word_dict[ws[j]], j] = 1
+
+    return t_par, t_c, t_ws        
+    
+
+q_word = 50
+q_par = 150
 
 t_c = tf.placeholder(tf.float32, shape=[T_word,1])       # 1-hot vector for central word
 t_word = tf.placeholder(tf.float32, shape=[T_word,None]) # 1-hot matrix for words
 t_par = tf.placeholder(tf.float32, shape=[T_par,1])      # 1-hot vector for pars
 
 # NPLM parameters
-W = tf.Variable(tf.random_uniform([p_word,T_word], -1.0, 1.0))
-D = tf.Variable(tf.random_uniform([p_par,T_par], -1.0, 1.0))
+W = tf.Variable(tf.random_uniform([q_word,T_word], -1.0, 1.0))
+D = tf.Variable(tf.random_uniform([q_par,T_par], -1.0, 1.0))
 
 # Context vectors 
 h_word = tf.matmul(W,t_word)
@@ -31,7 +60,7 @@ h_par = h_par[:,1]
 h = tf.concat(0,[h_word,h_par])
 
 # Softmax sizes
-p = p_word + p_par
+p = q_word + q_par
 T = T_word
 
 h = tf.reshape(h,[p,1])
