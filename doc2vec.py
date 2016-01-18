bfaf9af83941b765c71a0a53ea83ec7457f68440
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-18 09:56:30 -0500

    Log softmax.

--- doc2vec.py
+++ doc2vec.py
@@ -14,14 +14,18 @@ def stochastic_batch(k, n=200):
 
     return t_ps, t_ws, t_cs
 
-# def softmax(M):
-#     exps = tf.exp(M)
-#     summed_rows = tf.reduce_sum(exps,1)
-    
-
+def logsoftmax(M):
+    # See https://en.wikipedia.org/wiki/LogSumExp
+    # LSE(v) = log(exp(v1) + ... + exp(vn))
+    # LSE(v) = v_s + log(exp(v1-vs) + ... + exp(vn - vs))
+    # where v_s = max(v1,...,vn)
+    # Do this for each column (hence the reshape to correctly broadcast)
+    x_star = tf.reshape(tf.reduce_max(M,1), [n,1])
+    logsumexp = tf.reshape(tf.log(tf.reduce_sum(tf.exp(M-x_star),1)),[n,1]) + x_star
+    return M - logsumexp
 
 def doc2vec(q_w, q_p, batch_size=200, steps=10000, db_limit=100):
-    global ps,ws,ts,T_w,T_p
+    global ps,ws,ts,T_w,T_p,n
 
     # Initialization
     print("Initializing computation variables...")
@@ -63,17 +67,19 @@ def doc2vec(q_w, q_p, batch_size=200, steps=10000, db_limit=100):
     U_bow = tf.Variable(tf.zeros([q_p,T_w]))
     b_bow = tf.Variable(tf.zeros([T_w]))
 
-    y1 = tf.nn.softmax(tf.matmul(h,U) + b) # Perform softmax with params (U,b) from `context' h
-    y2 = tf.gather(tf.transpose(y1), t_cs)  # Evaluate the probabilities of target t_cs of each example
-    y3 = tf.log(y2)
-    y4 = tf.mul(mask,y3)                    # Mask elements off diagonal
+    # y = tf.nn.softmax(tf.matmul(h,U) + b) # Perform softmax with params (U,b) from `context' h
+    y = logsoftmax(tf.matmul(h,U) + b)
+    y = tf.gather(tf.transpose(y), t_cs)  # Evaluate the probabilities of target t_cs of each example
+    # y = tf.log(y)
+    y = tf.mul(mask,y)                    # Mask elements off diagonal
 
-    y_bow = tf.nn.softmax(tf.matmul(h_bow,U_bow) + b_bow)
+    # y_bow = tf.nn.softmax(tf.matmul(h_bow,U_bow) + b_bow)
+    y_bow = logsoftmax(tf.matmul(h_bow,U_bow) + b_bow)
     y_bow = tf.gather(tf.transpose(y_bow), t_cs)
-    y_bow = tf.log(y_bow)
+    # y_bow = tf.log(y_bow)
     y_bow = tf.mul(mask,y_bow)
 
-    cost = -tf.reduce_sum(y4)
+    cost = -tf.reduce_sum(y)
     cost_bow = -tf.reduce_sum(y_bow)
 
     train = tf.train.GradientDescentOptimizer(0.1).minimize(cost)
@@ -85,18 +91,20 @@ def doc2vec(q_w, q_p, batch_size=200, steps=10000, db_limit=100):
 
         print("Beginning SGD operation...")
         avg_cost = np.array([0.,0.])
+        feed = dict(zip([t_ps, t_ws, t_cs], stochastic_batch(k=4,n=n)))
         for i in range(steps):
-            feed = dict(zip([t_ps, t_ws, t_cs], stochastic_batch(k=4,n=n)))
+            # feed = dict(zip([t_ps, t_ws, t_cs], stochastic_batch(k=4,n=n)))
             _,_,cost_val,cost_bow_val = sess.run([train,train_bow,cost,cost_bow], feed)
             avg_cost += [cost_val,cost_bow_val]
 
+            print("Cost at step %d: (%f,%f)" % (i,avg_cost[0],avg_cost[1]))
             if math.isnan(avg_cost[0]) or math.isnan(avg_cost[1]):
                 raise ValueError("FUcking nan. Step %d" % i)
 
-            if i % 2000 == 0:
-                if i > 0:
-                    avg_cost /= [2000.,2000.]
-                print("Average losses at step %d: (%f,%f)" % (i,avg_cost[0],avg_cost[1]))
-                avg_cost = np.array([0.,0.])
+            # if i % 2000 == 0:
+            #     if i > 0:
+            #         avg_cost /= [2000.,2000.]
+            #     print("Average losses at step %d: (%f,%f)" % (i,avg_cost[0],avg_cost[1]))
+            #     avg_cost = np.array([0.,0.])
         
     print("DONE")
