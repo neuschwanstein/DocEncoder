120b9f54d29e6bfaae7e28904078e0513e392fa5
Thierry BM <thierry.bazier-matte@hec.ca>
2016-01-21 21:20:12 -0500

    Working!.

--- doc2vec.py
+++ doc2vec.py
@@ -13,6 +13,10 @@ import articles
 def doc2vec(q_w, q_p, batch_size=200, steps=10000, k=12, db_limit=100):
     global ps,ts,T_w,T_p,n
 
+    n = batch_size
+    num_sampled_dmm = 64
+    num_sampled_bow = 64
+
     ps,ts = initialize(k,db_limit)
     T_w = len(ts)
     T_p = len(ps)
@@ -24,46 +28,41 @@ def doc2vec(q_w, q_p, batch_size=200, steps=10000, k=12, db_limit=100):
     D_dmm = tf.Variable(tf.random_uniform([T_p, q_p], -1.0, 1.0),name='D_dmm')
 
     # Index placeholders
-    t_cs_dmm = tf.placeholder(tf.int32, shape=[n])
+    t_cs_dmm = tf.placeholder(tf.int32, shape=[n,1])
     t_ps_dmm = tf.placeholder(tf.int32, shape=[n])
     t_ws_dmm = tf.placeholder(tf.int32, shape=[n,None])
 
     # Context vector (from surrounding ws and p)
-    h_ps = tf.gather(D_dmm, t_ps_dmm)
-    h_ws = tf.gather(W_dmm, t_ws_dmm)
+    h_ps = tf.gather(D_dmm, t_ps_dmm,name='h_ps_gather')
+    h_ws = tf.gather(W_dmm, t_ws_dmm,name='h_ws_gather')
     h_ws = tf.reduce_mean(h_ws,1)   # `mix' context words (average in this case)
     h_dmm = tf.concat(1, [h_ps,h_ws])
 
     # Softmax parameters for PVDM movdel
-    U_dmm = tf.Variable(tf.zeros([q_w+q_p,T_w]),name='U_dmm')
+    U_dmm = tf.Variable(tf.zeros([T_w,q_w+q_p]),name='U_dmm')
     b_dmm = tf.Variable(tf.zeros([T_w]),name='b_dmm')
 
-    mask_dmm = tf.diag(tf.ones([n]))
-    y_dmm = logsoftmax(tf.matmul(h_dmm,U_dmm) + b_dmm)
-    y_dmm = tf.gather(tf.transpose(y_dmm), t_cs_dmm)  # Evaluate the probabilities of target
-                                                  # t_cs of each example (use sparse tensor?)
-    y_dmm = tf.mul(mask_dmm,y_dmm)                # Mask elements off diagonal
-
-    cost_dmm = -tf.reduce_sum(y_dmm)
+    cost_dmm = tf.reduce_mean(
+        tf.nn.sampled_softmax_loss(U_dmm,b_dmm,h_dmm,t_cs_dmm,num_sampled_dmm,T_w))
     train_dmm = tf.train.GradientDescentOptimizer(0.1).minimize(cost_dmm)
 
     # Distributed bag of words
-    D_bow = tf.Variable(tf.random_uniform([T_p,q_p], -1.0, 1.0))
+    D_bow = tf.Variable(tf.random_uniform([T_p,q_p], -1.0, 1.0),name='D_bow')
 
     # Index placeholders
     t_ps_bow = tf.placeholder(tf.int32, shape=[n])
-    t_css_bow = tf.placeholder(tf.int32, shape=[n,2*k]) # None bcz len(par) might be shorter
+    t_css_bow = tf.placeholder(tf.int32, shape=[n,2*k])
     
     # PVBOW vector
-    h_bow = tf.gather(D_bow, t_ps_bow) # [n,q_p]
+    h_bow = tf.gather(D_bow, t_ps_bow,name="my_gather_motherfucker") # [n,q_p]
 
     # Softmax paramaters for PVDBOW model
-    U_bow = tf.Variable(tf.zeros([q_p,T_w]))
-    b_bow = tf.Variable(tf.zeros([T_w]))
+    U_bow = tf.Variable(tf.zeros([T_w,q_p]),name='U_bow')
+    b_bow = tf.Variable(tf.zeros([T_w]),name='b_bow')
 
     num_sampled = 64            # How much??
     cost_bow = tf.reduce_mean(
-        tf.nn.sampled_softmax_loss(U_bow,b_bow,h_bow,t_css_bow,num_sampled,T_w,2*k))
+        tf.nn.sampled_softmax_loss(U_bow,b_bow,h_bow,t_css_bow,num_sampled_bow,T_w,2*k))
     train_bow = tf.train.GradientDescentOptimizer(0.1).minimize(cost_bow)
 
     init = tf.initialize_all_variables()
@@ -76,7 +75,7 @@ def doc2vec(q_w, q_p, batch_size=200, steps=10000, k=12, db_limit=100):
         for i in range(steps):
             # PVDM training
             feed_dmm = dict(zip(
-                [t_ps,t_ws,t_cs,t_cs_bow], stochastic_batch_dmm(k,n)))
+                [t_ps_dmm,t_ws_dmm,t_cs_dmm], stochastic_batch_dmm(k,n)))
             new_cost_dmm,_ = sess.run([cost_dmm,train_dmm],feed_dmm)
             
             # BOW Training
@@ -108,6 +107,7 @@ def stochastic_batch_dmm(k, n=200):
     t_ps = [random.randrange(T_p) for _ in range(n)]
     cs = [random.randrange(k, len(ps[t_p])-k) for t_p in t_ps]
     t_cs = [ts[ps[t_p][c]] for c,t_p in zip(cs,t_ps)]
+    t_cs = np.array(t_cs).reshape((len(t_ps),1))
     t_ws = [[ts[w] for w in ps[t_p][c-k:c]+ps[t_p][c+1:c+k+1]] for c,t_p in zip(cs,t_ps)]
     return t_ps, t_ws, t_cs
 
@@ -123,10 +123,6 @@ def stochastic_batch_bow(k, n=200):
     css = [[random.randrange(k,l+k) for _ in range(2*k)] for l in max_ls]
     t_css = [[ts[ps[t_p][c]] for c in cs] for cs,t_p in zip(css,t_ps)]
     return t_ps,t_css
-    # counters = [collections.Counter(sorted(t_cs)) for t_cs in t_css]
-    # mask = [list(counter.keys()) for counter in counters]
-    # t_css = [list(counter.values()) for counter in counters]
-    # return t_ps,mask,t_css
 
 def logsoftmax(M):
     '''LSE(v) = log(exp(v1) + ... + exp(vn))
